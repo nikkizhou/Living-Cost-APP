9bf68e0e7ebf56b97e858db755932d5cfa04dc9e
unknown <nikkizhou99@gmail.com>
2022-12-31 01:21:33 +0100

    refactored

--- server/db.tsx
+++ server/db.tsx
@@ -5,7 +5,7 @@ async function run() {
   await mongoose.connect(process.env.MONGO_KEY, { useNewUrlParser: true, useUnifiedTopology: true });
   console.log("MongoDB connected");
   const db = mongoose.connection;
-  // 4. Connect to MongoDB
+  // 4. Connect to MongoDB 
 
 
   const city = new City({
